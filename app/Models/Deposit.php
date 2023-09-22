2d3a40623f54b570b27ed855e05fa8d5ab0fce04
101 <info@101mirror.com>
2023-09-22 10:43:35 +0400

    Make modification on MODELS -- Car, Customer, Deposit, DepositValue, Discount, Reservation, ReservationPrice

--- app/Models/Deposit.php
+++ app/Models/Deposit.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Deposit extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public $timestamps = false;
+
+    public function reservation(): BelongsTo
+    {
+        return $this->belongsTo(Reservation::class);
+    }
 }
